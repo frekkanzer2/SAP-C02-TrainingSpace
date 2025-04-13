eeaa03f65209b066318b5b36032ddae722986f12
frekkanzer2 <abatefrancesco98@gmail.com>
2025-04-14 01:53:37 +0200

    New sh files for buckets management

---
+++ s3/download.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+if [ $# -lt 2 ] || [ "$1" == "help" ]; then
+    echo "INFO > You must call '$0 <BUCKET_NAME> <FILE_NAME> <OPT_PROFILE>'"
+    exit 1
+fi
+
+BUCKET_NAME=$1
+FILE_NAME=$2
+PROFILE=$3
+REGION="eu-south-1"
+
+AWS_COMMAND="aws s3 cp s3://$BUCKET_NAME/$FILE_NAME ./downloads/ --region $REGION"
+
+if [ -n "$PROFILE" ]; then
+    AWS_COMMAND="$AWS_COMMAND --profile $PROFILE"
+fi
+
+echo "EXEC > $AWS_COMMAND"
+sh -c "$AWS_COMMAND"
--- s3/remove-bucket.sh
+++ s3/remove-bucket.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
 
 if [ $# -lt 1 ] || [ "$1" == "help" ]; then
-    echo "INFO > You must call '$0 <BUCKET_NAME> <OPT_PROFILE>'"
+    echo "INFO > You must call '$0 <BUCKET_NAME> <OPT_FORCED|AGREE_WITH_y> <OPT_PROFILE>'"
     exit 1
 fi
 
 BUCKET_NAME=$1
 REGION="eu-south-1"
-PROFILE=$2
+FORCED=$2
+PROFILE=$3
 
 AWS_COMMAND="aws s3 rb s3://$BUCKET_NAME --region $REGION"
 
 if [ -n "$PROFILE" ]; then
     AWS_COMMAND="$AWS_COMMAND --profile $PROFILE"
+fi
+if [ -n "$FORCED" ] && [ "$FORCED" == "f" ]; then
+    AWS_COMMAND="$AWS_COMMAND --force"
 fi
 
 echo "EXEC > $AWS_COMMAND"
